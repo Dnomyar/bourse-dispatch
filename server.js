e4406f7bd355a90e6737321c9109425cfa0581a6
Thomas Senez <senezth@gmail.com>
2017-05-15 15:14:08 +0200

    getBourses method

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var http = require('http');
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+const request = require('request');
 
-var listener = 80;
+var listener = 83;
 
 var server = http.createServer(app);
 server.listen(listener);
@@ -21,3 +22,18 @@ app.get('/', function(req, res) {
     res.status(200);
     res.send("Test OK");
 });
+
+
+var getBourses = function(){
+    request({
+        method : 'GET',
+        uri : 'http://swarmjpg.westeurope.cloudapp.azure.com:8082/bourses',
+    },
+    function (error, response, body){
+        if (error){
+            return console.error('GET Failed :', error);
+        } 
+        console.log(response.statusCode);
+        return body;
+    });
+};
