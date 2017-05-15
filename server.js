259e316084e5b2ea6ef08d52a7609f2ac3bceb5a
Thomas Senez <senezth@gmail.com>
2017-05-15 15:36:05 +0200

    request 2.0

--- server.js
+++ server.js
@@ -18,22 +18,19 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get('/', function(req, res) {
+app.get('/demandes-bourses', function(req, res) {
     res.status(200);
-    res.send("Test OK");
-});
-
-
-var getBourses = function(){
-    request({
+    return request({
         method : 'GET',
-        uri : 'http://swarmjpg.westeurope.cloudapp.azure.com:8082/bourses',
+        uri : 'http://swarmjpg.westeurope.cloudapp.azure.com:8082',
     },
     function (error, response, body){
         if (error){
             return console.error('GET Failed :', error);
         } 
         console.log(response.statusCode);
-        return body;
+        var content = body;
+        // Transformations sur content
+        res.send(content);
     });
-};
+});
